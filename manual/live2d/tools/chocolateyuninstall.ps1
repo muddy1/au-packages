bfbfb20d1af24f87ebd5b936ad92ba5d260140e2
muddy1 <c0wf3d@gmail.com>
2025-04-03 11:39:25 -0400

    spring clean
    good housekeeping

---
+++ manual/live2d/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://cubism.live2d.com/editor/bin/Live2D_Cubism_Setup_5.2.02.exe'
+$fileName = [System.IO.Path]::GetFileName($url)
+$fileLocation = Join-Path $toolsDir $fileName
+$checksum = '73730B93063FD00B6803363AA8C58EB5FB5F36B9E9EF5298EFA2C86844C46F9F'
+
+$webFileArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileFullPath = $fileLocation
+  url          = $url
+  checksum     = $checksum
+  checksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @webFileArgs
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'live2d'
+  fileType       = 'EXE'
+  file           = $fileLocation
+  silentArgs     = '/S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
---
+++ manual/vortex/update.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module chocolatey-au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^\`$url\s*=\s*)('.*')"    = "`$1'$($Latest.Url32)'"
+            "(^\`$checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $token = $env:
+    $headers = @{
+        'User-Agent' = 'muddy1'
+    }
+    if ($token) {
+        $headers['Authorization'] = ("token {0}" -f $token)
+    } else {
+        Write-Warning "No auth token"
+    }
+
+    $releasesUrl = "https://api.github.com/repos/Nexus-Mods/vortex/releases/latest"
+    $response = Invoke-RestMethod -Method Get -Uri "$releasesUrl/latest" -Headers $headers
+    
+    # Assume releases are named v1.2.3
+    if (!$response.tag_name.StartsWith("v")) {
+        return @{}
+    }
+
+    $version = $response.tag_name.Substring(1)
+
+    $assets = Invoke-RestMethod -Method Get -Uri $response.assets_url -Headers $headers
+
+
+    $asset32 = $assets | Where-Object { $_.name -eq "vortex-setup-($version).exe" }
+
+    $Latest = @{
+        Url32 = $asset32.browser_download_url
+        Version = $version
+    }
+    return $Latest
+}
+
+update
