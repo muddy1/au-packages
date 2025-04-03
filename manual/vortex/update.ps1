81f314efeca874d0a295d60e9c0e1668abe42a15
muddy1 <c0wf3d@gmail.com>
2025-04-03 11:46:08 -0400

    Update update.ps1
    auto update prep

--- manual/vortex/update.ps1
+++ manual/vortex/update.ps1
@@ -1,45 +1,43 @@
-﻿Import-Module chocolatey-au
+﻿Import-Module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$repoUser = "Nexus-Mods"
+$repoName = "Vortex"
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^\`$url\s*=\s*)('.*')"    = "`$1'$($Latest.Url32)'"
-            "(^\`$checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-        }
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUri)>"
+      "(?i)(^\s*url(32)?\:\s*).*"         = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum(32)?\:\s*).*"    = "`${1}$($Latest.Checksum32)"
+      "(?i)(^\s*checksum\s*type\:\s*).*"  = "`${1}$($Latest.ChecksumType32)"
     }
+  }
+}
+
+function global:au_BeforeUpdate($Package) {
+  $licenseData = Get-GithubRepositoryLicense $repoUser $repoName
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { Remove-Item -Force $licenseFile }
+
+  Invoke-WebRequest -Uri $licenseData.download_url -UseBasicParsing -OutFile "$licenseFile"
+  $Latest.LicenseUrl = $licenseData.html_url
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_AfterUpdate($Package) {
+  Update-Metadata -key "licenseUrl" -value $Latest.LicenseUrl
+  Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
-    $token = $env:
-    $headers = @{
-        'User-Agent' = 'muddy1'
-    }
-    if ($token) {
-        $headers['Authorization'] = ("token {0}" -f $token)
-    } else {
-        Write-Warning "No auth token"
-    }
+  $release = Get-LatestGithubReleases $repoUser $repoName $true
 
-    $releasesUrl = "https://api.github.com/repos/Nexus-Mods/vortex/releases/latest"
-    $response = Invoke-RestMethod -Method Get -Uri "$releasesUrl/latest" -Headers $headers
-    
-    # Assume releases are named v1.2.3
-    if (!$response.tag_name.StartsWith("v")) {
-        return @{}
-    }
+  $url32 = $release.latest.Assets | Where-Object { $_ -match '\.exe$' } | Select-Object -First 1
 
-    $version = $response.tag_name.Substring(1)
-
-    $assets = Invoke-RestMethod -Method Get -Uri $response.assets_url -Headers $headers
-
-
-    $asset32 = $assets | Where-Object { $_.name -eq "vortex-setup-($version).exe" }
-
-    $Latest = @{
-        Url32 = $asset32.browser_download_url
-        Version = $version
-    }
-    return $Latest
+  $Latest = @{ URL32 = $url32; Version = $release.latest.Version; ReleaseUri = $release.latest.ReleaseUrl }
+  return $Latest
 }
 
-update
+update -ChecksumFor none
