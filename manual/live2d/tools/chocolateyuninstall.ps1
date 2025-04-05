2bf7142ab4ebdf70fb91fb3445c88108af5eb0e4
muddy1 <c0wf3d@gmail.com>
2025-04-05 14:45:11 -0400

    repair
    install uninstall repair

--- manual/live2d/tools/chocolateyuninstall.ps1
+++ manual/live2d/tools/chocolateyuninstall.ps1
@@ -1,27 +1,32 @@
-﻿﻿$ErrorActionPreference = 'Stop'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://cubism.live2d.com/editor/bin/Live2D_Cubism_Setup_5.2.02.exe'
-$fileName = [System.IO.Path]::GetFileName($url)
-$fileLocation = Join-Path $toolsDir $fileName
-$checksum = '73730B93063FD00B6803363AA8C58EB5FB5F36B9E9EF5298EFA2C86844C46F9F'
-
-$webFileArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  fileFullPath = $fileLocation
-  url          = $url
-  checksum     = $checksum
-  checksumType = 'sha256'
-}
-
-Get-ChocolateyWebFile @webFileArgs
-
+﻿$ErrorActionPreference = 'Stop'
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = 'live2d'
-  fileType       = 'EXE'
-  file           = $fileLocation
-  silentArgs     = '/S'
-  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'live2d*'
+  fileType      = 'EXE'
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
-Uninstall-ChocolateyPackage @packageArgs
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+
+    if ($packageArgs['fileType'] -eq 'EXE') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+
+      $packageArgs['file'] = ''
+    } else {
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
