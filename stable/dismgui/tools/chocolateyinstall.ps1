98273efacea6ffb77d2bfc4f3adbd8d1ef9684f2
muddy1 <c0wf3d@gmail.com>
2025-05-05 08:41:06 -0400

    added packages

---
+++ stable/dismgui/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://github.com/mikecel79/DISMGUI/releases/download/4.0/DISM.GUI.zip'
+  checksum       = '0DA4B2F08424CECA3EA1F4996566366B27C9B5A6E1C0EF37E89BA1224BF91041'
+  checksumType   = 'sha256'
+  unzipLocation  = "c:\Program Files\dismgui"
+  softwareName   = 'dismgui'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stable/dismgui/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'dism.gui.zip'
+
+# Uninstall-ChocolateyZipPackage will remove the FILES from the archive.
+# If you wish to remove the DIRECTORY they were extracted too,
+# you'll additionally have to handle that in this script.
+Remove-Item "c:\Program Files\dismgui"
